58a3319d2bef7a1abd1340cba1760a6c2a59aee2
Vitor Calegari <vitorprca@gmai.com>
2023-04-20 17:21:00 -0300

    Atualiza código para conseguir aumentar o número de bits da interface do demux

--- RelatorioPratico1/demux1para4/demux1para4.vhd
+++ RelatorioPratico1/demux1para4/demux1para4.vhd
@@ -3,11 +3,11 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY demux1para4 IS
-
+	GENERIC (N : POSITIVE := 2);
 	PORT (
-			f : IN std_logic;
+			f : IN std_logic_vector(N - 1 DOWNTO 0);
 			sel : IN std_logic_vector (1 DOWNTO 0);
-			a, b, c, d : OUT std_logic
+			a, b, c, d : OUT std_logic_vector(N - 1 DOWNTO 0)
 			);
 			
 END demux1para4;
@@ -16,9 +16,9 @@ ARCHITECTURE arch OF demux1para4 IS
 
 	BEGIN
 	
-	a <= f when sel = "00" else '0';
-	b <= f when sel = "01" else '0';
-	c <= f when sel = "10" else '0';
-	d <= f when sel = "11" else '0';
+	a <= f when sel = "00" else (others => '0');
+	b <= f when sel = "01" else (others => '0');
+	c <= f when sel = "10" else (others => '0');
+	d <= f when sel = "11" else (others => '0');
 	
 END arch;
